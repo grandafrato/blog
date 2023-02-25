c969e1fd7eb3e0cdca7c7050b7cc9c1a9ce8ee47
Lachlan Wilger <lachlan@wilgerfamily.com>
2023-02-25 14:27:36 -0800

    Changed build.rs to install node deps

--- build.rs
+++ build.rs
@@ -1,15 +1,38 @@
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("npx")
-        .args([
-            "tailwindcss",
-            "-i",
-            "tailwind_input.css",
-            "-o",
-            "assets/css/style.css",
-        ])
-        .output()
-        .expect("failed to execute process");
-    println!("{:?}", output);
+    cargo_build_config();
+    install_node_deps_if_necessary();
+    compile_tailwindcss();
+}
+
+fn install_node_deps_if_necessary() {
+    if !Path::new("node_modules/")
+        .try_exists()
+        .expect("Can't check for existence of node_modules")
+    {
+        let status = Command::new("npm")
+            .arg("install")
+            .status()
+            .expect("failed to exectute npm install");
+
+        assert!(status.success());
+    }
+}
+
+fn compile_tailwindcss() {
+    let status = Command::new("npx")
+        .arg("tailwindcss")
+        .args(["-i", "tailwind_input.css"])
+        .args(["-o", "assets/css/style.css"])
+        .status()
+        .expect("failed to execute npx tailwindcss");
+
+    assert!(status.success());
+}
+
+fn cargo_build_config() {
+    println!("cargo:rerun-if-changed=tailwind_input.css");
+    println!("cargo:rerun-if-changed=package.json");
 }
